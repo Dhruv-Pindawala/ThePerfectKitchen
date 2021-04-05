135e745903c6e106db0a8fc30a8bf0936dcc2f02
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-04-06 02:11:30 +0530

    footer referenced to footer.php

--- blog.php
+++ blog.php
@@ -77,15 +77,7 @@
     </div>
     <!-- /.container -->
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Company 2013</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+<?php require_once "footer.php"; ?>
 
     <!-- JavaScript -->
     <script src="js/jquery-1.10.2.js"></script>
