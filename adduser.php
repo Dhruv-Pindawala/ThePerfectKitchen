ce6d3f856d16ff2acc7630bb82a693125a6375ac
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-04-06 20:03:37 +0530

    check if new accounct is not created by a previously used email id

--- adduser.php
+++ adduser.php
@@ -25,6 +25,31 @@ if (strlen($fname) < 2) {
 
 } else {
     $spassword = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+
+    $query = "SELECT * FROM members WHERE email='$email'";
+    $result = mysqli_query($mysqli, $query) or die(mysqli_error());
+    $num_row = mysqli_num_rows($result);
+    $row = mysqli_fetch_array($result);
+
+    if ($num_row < 1) {
+
+        $insert_row = $mysqli->query("INSERT INTO members (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')");
+
+        if ($insert_row) {
+
+            $_SESSION['login'] = $mysqli->insert_id;
+            $_SESSION['fname'] = $fname;
+            $_SESSION['lname'] = $lname;
+
+            echo 'true';
+        }
+
+    } else {
+
+        echo 'false';
+
+    }
+    
 }
 
 ?>
