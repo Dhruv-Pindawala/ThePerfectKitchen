dc1dbbb0c1b095539d1d0ca363323b56cdea1196
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-04-06 19:30:53 +0530

    sql injection prevention

--- adduser.php
+++ adduser.php
@@ -7,4 +7,9 @@ if ($mysqli->connect_error) {
     die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
+$fname = mysqli_real_escape_string($mysqli, $_POST['fname']);
+$lname = mysqli_real_escape_string($mysqli, $_POST['lname']);
+$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+$password = mysqli_real_escape_string($mysqli, $_POST['password']);
+
 ?>
