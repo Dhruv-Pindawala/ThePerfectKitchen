279baa63e58c0223ee09f698a250d27859958ffe
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-04-06 18:36:24 +0530

    use of ajax for input validation

--- register.php
+++ register.php
@@ -14,6 +14,78 @@
 
     <!-- Add custom CSS here -->
     <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- JQuery -->
+    <script src="js/jquery-1.10.2.js"></script>
+
+    <!-- Script -->
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#register").click(function () {
+
+                fname = $("#fname").val();
+                lname = $("#lname").val();
+                email = $("#email").val();
+                password = $("password").val()
+
+                $.ajax({
+                    type: "POST",
+                    url: "adduser.php"
+                    data: "fname=" + fname +"&lname=" + lname + "&email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> \
+                                                 <strong>Account</strong> processed. \ \
+                                                 </div>');
+                            window.location.href = "index.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> already in system. \ \
+                                                 </div>');
+
+                        } else if (html == 'fname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>First Name</strong> is required. \ \
+                                                 </div>');
+                        
+                        } else if (html == 'lname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Last Name</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eformat') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> format is not valid. \ \
+                                                 </div>');
+
+                        } else if (html == 'pshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Password</strong> must be atleast 4 characters long. \ \
+                                                 </div>');
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Error</strong> processing request. Please try again. \ \
+                                                 </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading....");
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+
 </head>
 
 <body>
@@ -28,6 +100,7 @@
                     <hr>
                     <h2 class="intro-text text-center">Registration <strong>form</strong>
                     </h2>
+                    <div id="add_err2"></div>
                     <hr>
                     <form role="form">
                         <div class="row">
@@ -49,7 +122,7 @@
                                 <input type="password" id="password" name="password" maxlength="25" class="form-control">
                             </div>
                             <div class="form-group col-lg-12">
-                                <button type="submit" class="btn btn-warning">Submit</button>
+                                <button type="submit" id="register" class="btn btn-warning">Submit</button>
                             </div>
                         </div>
                     </form>
@@ -63,7 +136,6 @@
 <?php require_once "footer.php"; ?>
 
     <!-- JavaScript -->
-    <script src="js/jquery-1.10.2.js"></script>
     <script src="js/bootstrap.js"></script>
 
 </body>
